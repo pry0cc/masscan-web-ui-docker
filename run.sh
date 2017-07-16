d4282c2e9d5c79ef49d463a9d0461e4dff11ea4e
pry0cc <pry0cc@protonmail.com>
2017-07-16 20:20:51 +0000

    added new scripts

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+docker build -t pry0cc/masscan-web .
 mkdir -p scans/scanned
 mkdir -p scans/done
-mkdir -p scans/config
+cp -r config scans/
 cp exclude.txt scans/
 cp scan.sh scans/
 docker run -d -p 5050:80 -v $(pwd)/scans:/app/scans --name masscan-web pry0cc/masscan-web
